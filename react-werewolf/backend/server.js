12787e1cf64cc8966bc98c912a17485a1a0d6bf9
unknown <dfern237@fiu.edu>
2020-10-01 00:58:17 -0400

    added test API and index.html file to server

--- react-werewolf/backend/server.js
+++ react-werewolf/backend/server.js
@@ -3,10 +3,17 @@ const cors = require('cors');
  
 const app = express();
 const port = process.env.PORT || 5000;
- 
+testAPIRouter = require("./routes/testAPI") 
+
 app.use(cors());
 app.use(express.json());
+app.use("/testAPI",testAPIRouter)
+
  
+app.get('/', function(req, res){
+    res.sendFile('index.html', {root : __dirname + '/'});
+  });
+
 app.listen(port,() => {
-    console.log(`Server is running on port: ${port}`);
+    console.log(`Server running on port: ${port}`);
 });
