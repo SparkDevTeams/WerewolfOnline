23f091cf38ed42f80ae42b09a5719a6fc6d26911
danay1999 <dfern237@fiu.edu>
2020-10-20 17:00:13 -0400

    Username field

--- react-werewolf/src/App.js
+++ react-werewolf/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./containers/homepage";
+import WaitingRoom from "./containers/waitingroom";
 import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
 
 class App extends Component {
@@ -26,6 +27,7 @@ class App extends Component {
         <div className="App" style={{ margin: 0, padding: 0 }}>
           <Switch>
             <Route exact path="/" component={HomePage} />
+            <Route exact path="/waitingroom" component={WaitingRoom}  />
           </Switch>
           </div>
       </Router>
