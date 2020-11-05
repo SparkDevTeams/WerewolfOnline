7c3fd9dd9d6ee46d4c7f18045d01adf57f4ff9df
Gaby Yanes Perez <gyane007@fiu.edu>
2020-11-05 14:08:01 -0800

    Waiting Room

--- react-werewolf/src/containers/waitingroom/index.js
+++ react-werewolf/src/containers/waitingroom/index.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from "react";
 import queryString from 'query-string';
 import io from 'socket.io-client';
-
+import sparkdevLogo from "./../images/SparkDev.png";
+import werewolfBackground1 from "./../images/werewolf 1.png";
+import './style.css';
 let socket;
-
 
 const WaitingRoom = ({ location }) => {
 
@@ -12,7 +13,6 @@ const WaitingRoom = ({ location }) => {
   const [users, setUsers] = useState('');
   const ENDPOINT = 'localhost:5000'
   
-
   useEffect(() => {
     const {username,room} = queryString.parse(location.search);
 
@@ -31,16 +31,53 @@ const WaitingRoom = ({ location }) => {
   }, [ENDPOINT, location.search])
 
     return (
-      <div className="WaitingRoom">
-       <div className="main-container">
-        <div className="background-container">
-            <div>{users[0]}</div> 
-            <div>{users[1]}</div> 
+       <div className="WaitingRoom">
+        <div className="">
+        <div className="werewolfbackground1">
+                <img src={werewolfBackground1} className = "werewolfbackground1" alt="village-img"></img>
+            </div>
+        </div>
+        <div className="header">
+        <img
+            src={sparkdevLogo}
+            alt="sparkdev-logo"
+            className="header-logo"
+          ></img>
+         
+        </div>
 
+  
+       <div className="table-container">
+          <h1 className="table-title">
+          There are {users.length} out of 10 users online
+
+    <table id="t01">
+     <tr>
+    <th>{users[0]}</th>
+    <th>{users[1]}</th>
+  </tr>
+  <tr>
+    <th>{users[2]}</th>
+    <th>{users[3]}</th>
+  </tr>
+  <tr>
+    <th>{users[4]}</th>
+    <th>{users[5]}</th>
+  </tr>
+  <tr>
+    <th>{users[6]}</th>
+    <th>{users[7]}</th>
+  </tr>  
+  <tr>
+    <th>{users[8]}</th>
+    <th>{users[9]}</th>
+  </tr>  
+</table>
+
+</h1>
         </div>
     
        </div>
-      </div>
     );
   }
 export default WaitingRoom;
